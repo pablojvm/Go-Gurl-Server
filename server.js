44c71fc0514375eae6188222a0d17fc49cc43474
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-05-23 18:58:19 +0200

    day-1

---
+++ server.js
@@ -0,0 +1,25 @@
+try {
+    process.loadEnvFile()
+} catch (error) {
+    console.warn("archivo .env no encontrdo, usando variables predeterminadas")
+}
+
+const jsonServer = require("json-server")
+
+// creamos el servidor con json-server
+const server = jsonServer.create()
+
+// aqui se añaden las configuraciones básicas de un servidor
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+
+// aqui se añaden todas las rutas básicas de CRUD de la data y gestiona la base de datos en el archivo db.json
+const routes = jsonServer.router("db.json")
+server.use(routes)
+
+const PORT = process.env.PORT || 5005
+
+server.listen(PORT, () => {
+  console.log(`JSON server ejecutandose en el puerto ${PORT}`)
+  console.log(`Acceso Local en url http://localhost:${PORT}`)
+})
